9dc09af462fba42b3e04e7d10019cc542a2f6400
Chynna Evans <chynna@uber.com>
2020-05-09 19:41:29 -0400

    Using class for pickNickname. Old version

--- chyn/src/App.js
+++ chyn/src/App.js
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef} from 'react';
 import './App.css';
+import FlipPage from 'react-flip-page';
+
+const nicknames = ['chynna', 'chyn', 'chy', 'chunna']
+
+function pickNickname(nicknames){
+
+  var rand = Math.random();
+  var i = Math.floor(rand * Math.floor(nicknames.length));
+
+  return nicknames[i]
+}
+
+class Header_old extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      nicknames: nicknames,
+      currentIndex: 0,
+    };
+    this.flippage = useRef(null);
+  }
+
+  updateNickname() {
+    if(this.state.currentIndex === this.state.nicknames.length - 1) {
+      this.setState({
+        currentIndex: 0
+      });
+    } else {
+      this.setState({
+        currentIndex: this.state.currentIndex + 1
+      });
+    }
+  }
+
+  goNext() {
+    this.flippage.gotoNextPage();
+  }
+
+  render() {
+    return(
+        <FlipPage loopForever="true" ref={(component) => {this.flippage = component;}}>
+        <article><div id="header"><h1>chyn</h1></div></article>
+          <article><div id="header"><h1>chunna</h1></div></article>
+        </FlipPage>
+    )
+
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => {this.flippage.gotoNextPage();},3000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Header_old />
   );
 }
 
